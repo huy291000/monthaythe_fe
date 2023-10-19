e4a492193f03600c00a8ecda5601fae8ecfb6ade
huy291000 <huyvippro2226@gmail.com>
2023-10-19 08:39:12 +0700

    Init project

---
+++ api/api/LaborProtectionShop-master/src/main/java/com/cosmos/entity/Image.java
@@ -0,0 +1,30 @@
+package com.cosmos.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="Images")
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name = "path")
+    private String path;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name ="productid")
+    private Product product;
+}
